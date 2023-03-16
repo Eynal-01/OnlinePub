856700bed212ea456d181c20af8ad209dd4a1ba1
Baxsh_cw07@itstep.edu.az <Baxsh_cw07@itstep.edu.az>
2023-03-17 02:08:40 +0400

    Commands done

---
+++ OnlinePub/Models/Beer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Converters;
+
+namespace OnlinePub.Models
+{
+    public class Beer : Entity
+    {
+        public string Name { get; set; }
+        public double Volume { get; set; }
+        public double BeerCost { get; set; }
+    }
+}
---
+++ OnlinePub/Repositories/FakeRepo.cs
@@ -0,0 +1,22 @@
+﻿using OnlinePub.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlinePub.Repositories
+{
+    public class FakeRepo
+    {
+        public List<Beer> GetAll()
+        {
+            return new List<Beer>
+            {
+                new Beer{Id=1, Name="Corona", Volume=3.5, BeerCost=20},
+                new Beer{Id=2, Name="Heineken", Volume=2.0, BeerCost=35},
+                new Beer{Id=3, Name="Budweiser", Volume=2.5, BeerCost=40}
+            };
+        }
+    }
+}
